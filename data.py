0d67b7593041e0938d2381476948bb6b5f66ce58
Faerbit <faerbit@gmail.com>
2014-05-05 22:22:46 +0200

    Added May to data.py.

--- data.py
+++ data.py
@@ -2,13 +2,39 @@ January = []
 February = []
 March = []
 April = []
-May = [
-        "der Arbeit",
-        "der Kartoffel",
-        "des rechteckigen Fensters",
-        "der Vier",
-        "des Datums"
-        ]
+May=[
+    "der Arbeit", 
+    "der Kartoffel",
+    "des rechteckigen Fensters",
+    "der Vier",
+    "des Datums",
+    "des Backofens",
+    "der Katze",
+    "des Käses",
+    "des Zettel auf der Toilette wechseln",
+    "des Muffins",
+    "des Wörterausdenkens",
+    "des Icons",
+    "der Nussmandelerdnusswalnusskreme",
+    "des Spülmittelkonzentrates",
+    "des Handtuches",
+    "der Induktion",
+    "der Feuerwehr",
+    "des Tages",
+    "des Messfehlers",
+    "des Käfers",
+    "der Mikrowelle",
+    "der Milch",
+    "der Unterhose",
+    "der Uhr",
+    "des Hundes",
+    "der duftenden Socken",
+    "des Spülens",
+    "der Tür",
+    "des Schicksals",
+    "des Tages",
+    "des Bachelors"
+]
 June = []
 July = []
 August = []
