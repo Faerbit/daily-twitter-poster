381f2eaba1aac1c7642626645b180ef48dc000a1
Faerbit <faerbit@gmail.com>
2014-05-25 18:55:56 +0200

    Update data for June.

--- data.py
+++ data.py
@@ -35,7 +35,38 @@ May=[
     "des Tages",
     "des Bachelors"
 ]
-June = []
+June = [
+    "des Johannisbeersaftes", #1
+    "der Europäischen Komission für sinnvolle Jahrestage", #2
+    "der Europäischen Komission für sinnlose Jahrestage", #3
+    "des Logoüberdenkens", #4
+    "des Betätigen von Lichtschaltern", #5
+    "des Lachens", #6
+    "des übermäßigen Alkoholkonsumes", #7
+    "des Pinguins", #8
+    "des Muffins", #9
+    "des Klaufens", #10
+    "der Couch", #11
+    "der Weltmeisterschaft", #12
+    "der schlechten Schullektüre", #13
+    "der Musik", #14
+    "der Kissenschlacht", #15
+    "der Banane", #16
+    "der Wasserbombenschlacht", #17
+    "des Patiententacklings", #18
+    "der Bestätigung", #19
+    "des Hamsters", #20
+    "des Hackfleisches", #21
+    "der Prokrastination", #22
+    "der Gürtelschnalle", #23
+    "des Salzes & Pfeffers", #24
+    "der Pizza", #25
+    "des Fleckens", #26
+    "des Speiseeises in der Mikrowelle", #27
+    "der Tortilla", #28
+    "der Paella", #29
+    "des Streichens", #30
+]
 July = []
 August = []
 September = []
