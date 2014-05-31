4e29ce4dbcc0392205614c95e84bb118881ccacd
Faerbit <faerbit@gmail.com>
2014-05-31 22:49:32 +0200

    Adding unittest to test splitting messages.

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 from data import data
+from bot import split_message
 
 class DataTests(unittest.TestCase):
 
@@ -10,6 +11,20 @@ class DataTests(unittest.TestCase):
         duplicates = set([x for x in data_ if data_.count(x) > 1])
         self.assertEqual(duplicates, {}, "There is duplicate data.")
 
+class SplitMessageTests(unittest.TestCase):
+
+    def test_one_message(self):
+        test_message = "This is a test string with less than 140 characters."
+        self.assertEqual([test_message], split_message(test_message))
+
+    def test_two_messages(self):
+        test_message =  ("This is a test string with more than 140 characters"
+            ". Adding useless words to make the string longer. This was not"
+            " enough we need even more words. Adding a few more words to make"
+            " it more distinct.")
+        test_message1 = test_message[:133] + "(1/2)"
+        test_message2 = test_message[:134] + "(2/2)"
+        self.assertEqual([test_message1, test_message2], split_message(test_message))
 
 if __name__ == "__main__":
     unittest.main()
