3bd574d7a39f3171b72d3cbb04d487dd8b35ba78
Faerbit <faerbit@gmail.com>
2014-05-31 22:26:32 +0200

    Modularized bot.py for unittesting.

--- bot.py
+++ bot.py
@@ -1,27 +1,39 @@
 from twython import Twython
 from datetime import date
 import os
-import data
+from data import data
 
-API_KEY=os.environ["API_KEY"]
-API_SECRET=os.environ["API_SECRET"]
+def main():
+    API_KEY=os.environ["API_KEY"]
+    API_SECRET=os.environ["API_SECRET"]
 
-ACCESS_TOKEN=os.environ["ACCESS_TOKEN"]
-ACCESS_TOKEN_SECRET=os.environ["ACCESS_TOKEN_SECRET"]
+    ACCESS_TOKEN=os.environ["ACCESS_TOKEN"]
+    ACCESS_TOKEN_SECRET=os.environ["ACCESS_TOKEN_SECRET"]
 
-twitter = Twython (API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-twitter.verify_credentials()
+    twitter = Twython (API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    twitter.verify_credentials()
 
-today = date.today()
+    today = date.today()
 
-message = "Heute ist der Tag "
+    message = "Heute ist der Tag "
 
-message = message + data.data[today.month-1][today.day-1] + "!"
+    message = message + data[today.month-1][today.day-1] + "!"
 
-if len(message) >140:
-    message1=message[:135] + "(1/2)"
-    twitter.update_status(status=message1)
-    message2=message[136:275] + "(2/2)"
-    twitter.update_status(status=message2)
-else:
-    twitter.update_status(status=message)
+    for status in split_message(message):
+        twitter.update_status(status=status)
+
+
+def split_message(message):
+    messages = []
+    if len(message) >140:
+        message1=message[:135] + "(1/2)"
+        messages.append(message1)
+        message2=message[136:275] + "(2/2)"
+        messages.append(message2)
+    else:
+        messages.append(message)
+    return messages
+
+
+if __name__ == "__main__":
+    main()
