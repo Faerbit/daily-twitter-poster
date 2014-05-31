ffad4375da20b88939a22105e3c93903d035954d
Faerbit <faerbit@gmail.com>
2014-06-01 00:00:44 +0200

    Adding another test which should produce three messages.

--- tests.py
+++ tests.py
@@ -26,5 +26,18 @@ class SplitMessageTests(unittest.TestCase):
         test_message2 = test_message[134:] + "(2/2)"
         self.assertEqual([test_message1, test_message2], split_message(test_message))
 
+    def test_three_messages(self):
+        test_message =  ("This is a test string with more than 280 characters"
+                ". Adding useless words to make the string longer. This was "
+                "not enough we need even more words. Mary had a little lamb. "
+                "His fleece was white as snow. And everywhere that Mary went, "
+                "the lamb was sure to go. He followed her to school one day. "
+                "Which was against the rule. It made the children laugh and "
+                "play, to see a lamb at school.")
+        test_message1 = test_message[:133] + "(1/3)"
+        test_message2 = test_message[134:267] + "(2/3)"
+        test_message3 = test_message[268:] + "(3/3)"
+        self.assertEqual([test_message1, test_message2, test_message3], split_message(test_message))
+
 if __name__ == "__main__":
     unittest.main()
