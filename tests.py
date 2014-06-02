66445d5b795592077f124defa666ad3d12f4441a
Faerbit <faerbit@gmail.com>
2014-06-02 20:36:54 +0200

    Fixing wrong variable name.

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from data import data
+from data import data_de
 from bot import TwitterBot
 from datetime import date, datetime
 import os
@@ -8,7 +8,7 @@ class DataTests(unittest.TestCase):
 
     def test_no_duplicates(self):
         data_ = []
-        for i in data:
+        for i in data_de:
             data_.extend(i)
         duplicates = set([x for x in data_ if data_.count(x) > 1])
         self.assertEqual(duplicates, {}, "There is duplicate data.")
